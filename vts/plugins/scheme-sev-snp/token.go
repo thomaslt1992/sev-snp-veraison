9cc931945e9d831d1a2f2a3090c9ecac4dd43583
thomaslt1992 <thomas.tasioulis92@gmail.com>
2022-09-14 14:22:48 +0300

    Encoding data structure

--- vts/plugins/scheme-sev-snp/token.go
+++ vts/plugins/scheme-sev-snp/token.go
@@ -1,19 +1,27 @@
 package main
 
+import (
+	"crypto/ecdsa"
+)
+
 // Token is the container for the decoded token in SEV-SNP environment
 type Token struct {
-	tcb_version        tcb_version
-	attestation_report attestation_report
-	// TODO: add certificates
+	tcb_version       TcbVersion
+	attestationReport AttestationReport
+	signature         Signature
 }
 
 type extended_attestation_report struct {
-	report attestation_report
-	//TODO: cert	certificate
+	report AttestationReport
+	cert   Certificate
 }
 
-type tcb_version struct {
-	tcb_union struct {
+type Certificate struct {
+	a int
+}
+
+type TcbVersion struct {
+	TcbUnion struct {
 		boot_loader *uint8
 		tee         *uint8
 		reserved    [4]*uint8
@@ -22,15 +30,16 @@ type tcb_version struct {
 	}
 	raw uint64
 }
-type attestation_report struct {
+
+type AttestationReport struct {
 	version           uint32
 	guest_svn         uint32
 	policy            uint64
 	family_id         [16]uint8
 	image_id          [16]uint8
 	vpml              uint32
-	signature_algo    uint32
-	platform_version  tcb_version
+	signatureAlgo     uint32
+	platform_version  TcbVersion
 	platform_info     uint64
 	flags             uint32
 	reserved0         uint32
@@ -41,24 +50,30 @@ type attestation_report struct {
 	author_key_digest [48]uint8
 	report_id         [32]uint8
 	report_id_ma      [32]uint8
-	reported_tcb      tcb_version
+	reported_tcb      TcbVersion
 	reserved1         [24]uint8
 	chip_id           [64]uint8
 	reserved2         [192]uint8
-	signature         signature
+	signature         Signature
 }
 
-type signature struct {
+type Signature struct {
 	r        [72]uint8
 	s        [72]uint8
 	reserved [512 - 144]uint8
 }
 
-/**
-type msg_report_resp struct {
+type MsgReportResponse struct {
 	status      uint32
 	report_size uint32
 	reserved    [0x20 - 0x8]uint8
-	report      attestation_report
+	report      AttestationReport
 }
-*/
+
+func (t Token) VerifySignature(key *ecdsa.PublicKey) error {
+	return nil
+}
+
+func (t Token) Decode(data []byte) error {
+
+}
